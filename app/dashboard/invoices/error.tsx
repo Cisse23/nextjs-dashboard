a34595bd908862d1879da85c1c5415bdd3faf3e8
Christel Baker <christel.baker@020202.fi>
2024-02-02 16:23:30 +0200

    chapter 14, accessibility and validation

--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -1,16 +1,17 @@
 'use client'
 
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 export default function Error({ 
     error, 
     reset, 
-} : {error: Error & { digest?: string };
+} : {
+    error: Error & { digest?: string };
     reset: () => void;
     }) {
         useEffect( () => {
             console.error(error);
-        }, [Error]);
+        }, [error]);
 
     return(
         <main className="flex h-full flex-col items-center justify-center">
