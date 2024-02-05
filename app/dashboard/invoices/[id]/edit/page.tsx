975ab758bd624a745aa4273bd190e5f85b6c251d
Christel Baker <christel.baker@020202.fi>
2024-02-05 16:34:15 +0200

    chapter 16, metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,10 +4,15 @@ and want to create routes based on data. This could be blog post titles, product
 You can create dynamic route segments by wrapping a folder's name in square brackets. 
 For example, [id], [post] or [slug].
 */
+import { Metadata } from "next";
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: 'Edit invoice',
+};
 
 export default async function Page( { params }:{ params: { id: string }} ) {
   const id = params.id;
